ce37c8bb0ead3acee73b19744457657e83388ee1
Yukihiro SAITO <yukky.saito@gmail.com>
2019-02-08 01:58:15 +0900

    bug fixed

--- src/prediction_model/foo_prediction_model.cpp
+++ src/prediction_model/foo_prediction_model.cpp
@@ -14,31 +14,31 @@ FooPredictionModel::FooPredictionModel(const Eigen::Vector3d &vel,
 {
 }
 
-
 bool FooPredictionModel::predict(State &state, const double dt_sec)
 {
-    const Eigen::Matrix3d rot_map2base_link = state.quat.normalized().toRotationMatrix();
-    const Eigen::Vector3d vel_map = rot_map2base_link.inverse() * vel_;
-    /* linear */
-    {
-        std::normal_distribution<double> noise(1.0, 1.0);
-        state.position.x() += (vel_map.x() * noise(rand_)) * dt_sec;
-        state.position.y() += (vel_map.y() * noise(rand_)) * dt_sec;
-        state.position.z() += (vel_map.z() * noise(rand_)) * dt_sec;
-    }
-    /* rotation */
-    {
-        std::normal_distribution<double> noise(0.0, 0.5);
-        Eigen::Matrix3d skew_omega;
-        skew_omega << 0.0, -omega_.z() + noise(rand_), omega_.y() + noise(rand_),
-            omega_.z() + noise(rand_), 0.0, -omega_.x() + noise(rand_),
-            -omega_.y() + noise(rand_), omega_.x() + noise(rand_), 0.0;
-        Eigen::Matrix3d rot_map2base_link_next = rot_map2base_link + rot_map2base_link * skew_omega * dt_sec;
-        rot_map2base_link_next.col(0) = rot_map2base_link_next.col(0).normalized();
-        rot_map2base_link_next.col(1) = rot_map2base_link_next.col(1).normalized();
-        rot_map2base_link_next.col(2) = rot_map2base_link_next.col(2).normalized();
-        state.quat = Quat(rot_map2base_link_next);
-    }
+    Position local_position;
+    Quat local_quat;
+    std::normal_distribution<double> vel_scale_noise(1.0, 1.0),
+        vel_bias_noise(0.0, 1.0),
+        omega_scale_noise(1.0, 0.5),
+        omega_bias_noise(0.0, 0.5);
+    double vel = vel_.x() * vel_scale_noise(rand_) + vel_bias_noise(rand_);
+    double omega = omega_.z() * omega_scale_noise(rand_) + omega_bias_noise(rand_);
+    const double r = vel / omega;
+    local_position.x() = r * std::sin(omega * dt_sec);
+    local_position.y() = -r * std::cos(omega * dt_sec) + r;
+    local_position.z() = 0.0;
+    local_quat = Quat(Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitX())                // roll
+                      * Eigen::AngleAxisd(0.0, Eigen::Vector3d::UnitY())              // pitch
+                      * Eigen::AngleAxisd(omega * dt_sec, Eigen::Vector3d::UnitZ())); // yaw
+
+    Eigen::Affine3d mat_global2local;
+    Eigen::Translation<double, 3> translation =
+        Eigen::Translation<double, 3>(state.position.x(), state.position.y(), state.position.z());
+    mat_global2local = translation * state.quat.normalized();
+    state.position = mat_global2local * local_position;
+
+    state.quat = (state.quat * local_quat).normalized();
     return true;
 }
 
